b15d7031143baa89d7a6aeec09b9bf9418589994
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-01-20 11:44:29 +0600

    if else if

---
+++ Conditions/If-Else-If.dart
@@ -0,0 +1,17 @@
+void main(){
+  int noOfMonth = 5;
+
+  if(noOfMonth == 1){
+    print("The month is jan");
+  } else if (noOfMonth == 2){
+    print("The month is feb");
+  } else if (noOfMonth == 3){
+    print("The month is march");
+  } else if (noOfMonth == 4){
+    print("The month is April");
+  } else if (noOfMonth == 5){
+    print("The month is may");
+  } else{
+    print('Invalid option given.');
+  }
+}
