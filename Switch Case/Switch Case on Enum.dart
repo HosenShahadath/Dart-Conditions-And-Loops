632036676ec9dd486b808c498eaf2f731eb309cf
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-01-21 10:27:38 +0600

    switch case on enum

---
+++ Switch Case/Switch Case on Enum.dart
@@ -0,0 +1,23 @@
+enum Weather{
+  sunny, snowy, cloudy, rainy
+}
+
+void main(){
+  const weather = Weather.cloudy;
+
+  switch(weather) {
+    case Weather.sunny:
+      print("Its a sunny day. Put sunscreen.");
+      break;
+    case Weather.snowy:
+      print("Get your skis.");
+      break;
+    case Weather.rainy:
+    case Weather.cloudy:
+      print("Please bring umbrella");
+      break;
+    default:
+      print("Invalid");
+      break;
+  }
+}
