041815e9c36e3e2c45ade749a9993ffd59ece18e
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-03-02 12:55:38 +0600

    question 7

---
+++ Question 2/question 7.dart
@@ -0,0 +1,13 @@
+void main(){
+
+  for(int x = 0; x < 9 ; x++ ){
+    print('Multiplication Table of $x');
+    for(int z = 1; z <= 10; z++){
+      print('$z * $z = ${x * z}');
+    }
+
+    print('');
+
+  }
+
+}
